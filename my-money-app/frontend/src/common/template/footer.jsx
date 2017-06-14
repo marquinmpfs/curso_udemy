23a85737eed81fbdde89281678f628315e910c5d
Marcos Ferreira <marcos.silva@eldorado.org.br>
2017-06-14 12:29:03 -0300

    Section 9 Lesson 108

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href="http://www.kakadoces.com.br" target="_blank">KakaDoces</a>
+        </strong>
+    </footer>
+)
