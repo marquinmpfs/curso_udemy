ddc373a99fd57686a9f5b626833887a5183c4b5f
Marcos Ferreira <marcos.silva@eldorado.org.br>
2017-06-26 11:55:36 -0300

    Section 10 Lesson 156

--- my-money-app/backend/src/config/cors.js
+++ my-money-app/backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
     next()
 }
--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,19 +5,31 @@ import { connect } from 'react-redux'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
+    calculateSummary(){
+        const sum = (total, values) => total + values
+        return{
+            sumOfCredits: this.props.credits.map(credit => +credit.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(debit => +debit.value || 0 ).reduce(sum)
+        }
+    }
+
     render() {
-        const {handleSubmit, readOnly, credits} = this.props
+        const {handleSubmit, readOnly, credits, debts} = this.props
+        const {sumOfCredits, sumOfDebts} = this.calculateSummary()
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
                     <Field name='name' component={LabelAndInput} label='Nome' cols='12 4' readOnly={readOnly} placeholder='Informe o nome'/>
                     <Field name='month' component={LabelAndInput} type='number' label='Mês' readOnly={readOnly} cols='12 4' placeholder='Informe o mês'/>
                     <Field name='year' component={LabelAndInput} type='number' label='Ano' readOnly={readOnly} cols='12 4' placeholder='Informe o ano'/>
-                    <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
+                    <Summary credit={sumOfCredits} debt={sumOfDebts}/>
+                    <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Créditos' />
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Débitos' showStatus={true} />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className={`btn btn-${this.props.submitClass}`}>
@@ -30,8 +42,11 @@ class BillingCycleForm extends Component {
     }
 }
 
-const selector = formValueSelector('billingCycleForm')
-const mapStateToProps = state => ({credits:selector(state, 'credits')})
-const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
 BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
+const selector = formValueSelector('billingCycleForm')
+const mapStateToProps = state => ({
+    credits:selector(state, 'credits'),
+    debts:selector(state, 'debts')
+})
+const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
